3e4e9effe4a10ef8e6b955245d0aadd6eea21f8f
三野田脩 <minodasyuu@sannodaosamunoMacBook-puro.local>
2018-09-22 14:58:10 +0900

    Add AddController

--- TodoApp/AddController.swift
+++ TodoApp/AddController.swift
@@ -7,6 +7,33 @@
 
 import UIKit
 
-class AddController: NSObject {
+class AddController: UIViewController {
+  
+  @IBOutlet weak var todoTextField: UITextField!
+  var todoList: [String] = []
+
+  @IBAction func todoAddButton(_ sender: Any) {
+    //何も入力されていなければ処理しない
+    guard let todoText = todoTextField.text else {
+      return
+    }
+
+    //何も登録されていなければ変数の最初に値を代入
+    if var todoList = UserDefaults.standard.stringArray(forKey: "todoList") {
+      todoList.append(todoText)
+    } else {
+      todoList[0] = todoText
+    }
+    UserDefaults.standard.set(todoList, forKey: "todoList")
+
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
 
 }
